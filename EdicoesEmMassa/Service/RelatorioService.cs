5669b695fe3682bc3b4a5d8d412a198911b981e5
Joao Marcos Carniel <joaomarcoscrr@gmail.com>
2022-11-29 20:37:22 -0300

    Cool System here we go

--- EdicoesEmMassa/Service/RelatorioService.cs
+++ EdicoesEmMassa/Service/RelatorioService.cs
@@ -4,7 +4,6 @@ using EdicoesEmMassa.Model.Reports;
 using EdicoesEmMassa.Repository;
 using iTextSharp.text;
 using iTextSharp.text.pdf;
-using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
